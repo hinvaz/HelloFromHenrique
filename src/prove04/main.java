6dce6917c925477021e803734067098a521e09a9
henriquevaz <vaz18004@byui.edu>
2019-10-10 20:59:07 -0600

    Revert "Henrique I changed your file!!!"
    This reverts commit f452bbeec7d6c8cb41ddce82ca3688d9670f5b28.

--- src/prove04/main.java
+++ src/prove04/main.java
@@ -4,10 +4,5 @@ public class main {
     public static void main(String[] args){
 
         System.out.println("Hello, my name is Henrique!");
-
-
-        System.out.println("I'm back!!! From Leo");
-
-
     }
 }
